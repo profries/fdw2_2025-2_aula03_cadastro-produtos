2205c6c5c79370ad3796d5c290a22ed9af4fab1b
Luis Henrique Ries <prof.ries@gmail.com>
2025-08-21 20:59:35 -0300

    Criacao do componente Tabela-Produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabelaProdutos } from "./tabela-produtos/tabela-produtos";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TabelaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cadastro-produtos');
 }
