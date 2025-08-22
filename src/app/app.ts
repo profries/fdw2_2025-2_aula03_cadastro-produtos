8a7dfedf1e3951ff168de19c434f9e35c9378b6d
Luis Henrique Ries <prof.ries@gmail.com>
2025-08-21 21:06:46 -0300

    Criacao do componente Form-Produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabelaProdutos } from "./tabela-produtos/tabela-produtos";
+import { FormProdutos } from "./form-produtos/form-produtos";
 
 @Component({
   selector: 'app-root',
-  imports: [TabelaProdutos],
+  imports: [TabelaProdutos, FormProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
